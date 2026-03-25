ebca9aa884fa949fd2dac7e7681c9bd46be79fa2
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2026-03-25 08:27:01 -0300

    config database local

--- server/db.ts
+++ server/db.ts
@@ -1,5 +1,6 @@
 import { and, desc, eq, like, or, sql, inArray, count } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
 import { InsertUser, users, posts, categories, postCategories, media, InsertPost, InsertCategory, InsertMedia } from "../drizzle/schema";
 import { ENV } from './_core/env';
 
@@ -8,7 +9,14 @@ let _db: ReturnType<typeof drizzle> | null = null;
 export async function getDb() {
   if (!_db && process.env.DATABASE_URL) {
     try {
-      _db = drizzle(process.env.DATABASE_URL);
+      const pool = mysql.createPool({
+        uri: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: true,
+        },
+      });
+
+      _db = drizzle(pool);
     } catch (error) {
       console.warn("[Database] Failed to connect:", error);
       _db = null;
