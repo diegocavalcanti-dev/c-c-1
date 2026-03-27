618da788578b2d82e12839224ea92d471b660553
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2026-03-27 19:03:42 -0300

    config ads

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
+import { useEffect } from "react";
+import { useLocation, Route, Switch } from "wouter";
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
@@ -14,26 +15,48 @@ import AdminPostEditor from "./pages/admin/AdminPostEditor";
 import AdminCategories from "./pages/admin/AdminCategories";
 import AdminImport from "./pages/admin/AdminImport";
 
+// Componente para notificar o AdSense sobre mudanças de rota no SPA
+function AdSenseTrack() {
+  const [location] = useLocation();
+
+  useEffect(() => {
+    try {
+      // @ts-ignore
+      if (window.adsbygoogle) {
+        // @ts-ignore
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+      }
+    } catch (e) {
+      // Ignora erros se o script ainda não tiver sido carregado pelo Google
+    }
+  }, [location]);
+
+  return null;
+}
+
 function Router() {
   return (
-    <Switch>
-      {/* Public routes */}
-      <Route path="/" component={Home} />
-      <Route path="/categoria/:slug" component={ArticleList} />
-      <Route path="/busca" component={SearchPage} />
-      <Route path="/:slug" component={ArticlePage} />
+    <>
+      <AdSenseTrack />
+      <Switch>
+        {/* Public routes */}
+        <Route path="/" component={Home} />
+        <Route path="/categoria/:slug" component={ArticleList} />
+        <Route path="/busca" component={SearchPage} />
+        <Route path="/:slug" component={ArticlePage} />
 
-      {/* Admin routes */}
-      <Route path="/admin" component={AdminDashboard} />
-      <Route path="/admin/posts" component={AdminPosts} />
-      <Route path="/admin/posts/novo" component={AdminPostEditor} />
-      <Route path="/admin/posts/:id/editar" component={AdminPostEditor} />
-      <Route path="/admin/categorias" component={AdminCategories} />
-      <Route path="/admin/importar" component={AdminImport} />
+        {/* Admin routes */}
+        <Route path="/admin" component={AdminDashboard} />
+        <Route path="/admin/posts" component={AdminPosts} />
+        <Route path="/admin/posts/novo" component={AdminPostEditor} />
+        <Route path="/admin/posts/:id/editar" component={AdminPostEditor} />
+        <Route path="/admin/categorias" component={AdminCategories} />
+        <Route path="/admin/importar" component={AdminImport} />
 
-      <Route path="/404" component={NotFound} />
-      <Route component={NotFound} />
-    </Switch>
+        <Route path="/404" component={NotFound} />
+        <Route component={NotFound} />
+      </Switch>
+    </>
   );
 }
 
